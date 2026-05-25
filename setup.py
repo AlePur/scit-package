e556e77129b4493479b4e2a8efe05dc8a5dbe324
AlePur <aleksander.purik@gmail.com>
2026-05-25 13:59:27 +0200

    fix src

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
-from setuptools import setup, Extension
+from setuptools import setup, find_packages
 from Cython.Build import cythonize
 import numpy
 
 setup(
+    # Explicitly find and map packages relative to 'src'
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    
     ext_modules=cythonize("src/**/*.pyx", annotate=False),
     include_dirs=[numpy.get_include()]
 )

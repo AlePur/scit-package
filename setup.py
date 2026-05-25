d0da6188f6a550febd864eb30f7f437b1b691ded
AlePur <aleksander.purik@gmail.com>
2026-05-25 13:55:53 +0200

    add np + isolation

--- setup.py
+++ setup.py
@@ -1,20 +1,8 @@
-from distutils.core import setup, Extension
+from setuptools import setup, Extension
 from Cython.Build import cythonize
-import sysconfig
 import numpy
-
-# Get the actual Python include path
-#python_include = sysconfig.get_path('include')
-#numpy_include = "/tmp/np-include"
 
 setup(
     ext_modules=cythonize("src/**/*.pyx", annotate=False),
     include_dirs=[numpy.get_include()]
-    #include_dirs=[numpy_include],
-    #include_dirs=[
-    #       "/home/aleksander/Skrivbord/pyproject/venv/bin/python",
-    #       "/home/aleksander/Skrivbord/pyproject/venv/lib/python3.12/site-packages/numpy/_core/include"
-    #],
-    #extra_compile_args=["-O3", "-march=native", "-fopenmp"],
-    #extra_link_args=["-fopenmp"],
 )
